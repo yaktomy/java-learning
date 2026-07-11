655a2ff8419dd78769087f495ba1746a47036a83
marselina <marselinazarifullina0@gmail.com>
2026-07-11 15:55:13 +0300

    Day 1: Java basics

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       double euro = 100;
+       double rate = 92.5;
+       double rubles = euro * rate;
+       System.out.println("100 евро в рублях:" + rubles);
+
+
+    }
+}
