1fee2716e5e24023b2827ea143c301517b7d27ef
marselina <marselinazarifullina0@gmail.com>
2026-07-15 22:29:37 +0300

    Повторение ООП

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
+    public static class BankAcount{
+        private String owner;
+        private double balance;
+        public BankAcount(){
+            this.owner = "Марселина";
+            this.balance = 1956;
+        }
+        public void printBalance(){
+            System.out.println(balance);
+        }
+        public void deposit(){
+            balance+= 1289;
+        }
+        public void withdraw(){
+            balance-= 167;
+        }
+    }
     public static void main(String[] args) {
-       double euro = 100;
-       double rate = 92.5;
-       double rubles = euro * rate;
-       System.out.println("100 евро в рублях:" + rubles);
-
-
+        BankAcount bankAcount = new BankAcount();
+        bankAcount.printBalance();
+        bankAcount.deposit();
+        bankAcount.printBalance();
+        bankAcount.withdraw();
+        bankAcount.printBalance();
     }
 }
+
