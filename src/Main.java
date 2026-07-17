cbab0719f1b5a8e426636fd31232b8975eb08bb2
marselina <marselinazarifullina0@gmail.com>
2026-07-17 19:12:22 +0300

    Day 6: main concepts OOP

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,56 @@
 import java.util.Scanner;
 public class Main {
-    public static class BankAcount{
-        private String owner;
-        private double balance;
-        public BankAcount(){
-            this.owner = "Марселина";
-            this.balance = 1956;
+    public static abstract class Employee{
+        String name;
+        double salary;
+        public Employee(String name, double salary){
+            this.name = name;
+            this.salary = salary;
         }
-        public void printBalance(){
-            System.out.println(balance);
+        public abstract void work();
+    }
+    public static class Developer extends Employee{
+        @Override
+        public void work(){
+            System.out.println("Разработчик "+ name +" пишет код");
         }
-        public void deposit(){
-            balance+= 1289;
+        public Developer(String name, double salary){
+            super(name,salary);
         }
-        public void withdraw(){
-            balance-= 167;
+
+
+    }
+    public static class Tester extends Employee{
+        @Override
+        public void work(){
+            System.out.println("Тестировщик "+ name +" тестирует приложение");
+        }
+        public Tester(String name, double salary){
+            super(name,salary);
+        }
+
+    }
+    public static class Manager extends Employee{
+        @Override
+        public void work(){
+            System.out.println("Менеджер "+ name +" проводит встречи");
+        }
+        public Manager(String name, double salary){
+            super(name,salary);
         }
     }
+    public static interface Payable{
+        void paySalary();
+    }
     public static void main(String[] args) {
-        BankAcount bankAcount = new BankAcount();
-        bankAcount.printBalance();
-        bankAcount.deposit();
-        bankAcount.printBalance();
-        bankAcount.withdraw();
-        bankAcount.printBalance();
+        Employee[] employees = {
+                new Developer("Марселина", 120000),
+                new Tester("Анна", 123333),
+                new Manager("Олег", 26378)
+        };
+        for (int i = 0; i< employees.length; i++){
+            employees[i].work();
+        }
     }
 }
 
